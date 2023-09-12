b65d28a3b835afb7222b0119699f8e8f4056c99c
arushisaxe <78280505+arushisaxe@users.noreply.github.com>
2023-09-11 23:59:26 -0400

    Time: 426 ms (10.24%), Space: 21.3 MB (10.33%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,16 +1,19 @@
 class Solution:
-    
-#0 need to get user input and create an instance of Solution to output
-#1 store_list = [] to hold qualifying substrings
-#2 cannot have any repeating characters > loop for this
-#3 1 character also qualifies
-#4 get the max length element in store_list
 
-    # pwwkew is returning pwke
-    # so when it reaches pww, your while loop does not end up breaking,
-    # it just says "in store_val" so continue through "j iteration for loop"
-    # basically need a way to break out of the j for loop
-    # OR combine the for loop with the while loop and then append
+# Initial Thought Process:
+# store_list = [] to hold qualifying substrings
+# cannot have any repeating characters > loop for this
+# 1 character also qualifies
+# get the max length element in store_list
+
+# Error: (Loop was not actually breaking at a repeated char)
+# ("abc" worked b/c those were the only 2 unique chars as well)
+
+# Error: "pwwkew" is returning "pwke"
+# so when it reaches pww, while loop does not end up breaking..
+# it just says "in store_val" so continue through "j iteration for loop"
+# basically need a way to break out of the j for loop
+# OR combine the for loop with the while loop and then append
     
     @staticmethod
     def lengthOfLongestSubstring(s: str):
@@ -39,9 +42,11 @@ class Solution:
   
         max_val = max(store_list, key=lambda x: len(x))
     
-    
-        # *don't like this approach of more code for "" and " "..
-        # need simpler solution 
+        '''
+        # *don't like this brute force sort of approach
+            # more code for "" and " "..
+        # *find a shorter solution later!
+        '''  
         
         if max_val == " ":
             return 1
@@ -50,13 +55,6 @@ class Solution:
             return max_length
 
 '''
-       
-# ohh so in this code: "pwwkew" is outputting "pwke" as one of the substrings
-# returning 4 instead of what should be 3
-# basically my program here is not going in order and breaking in between
-# it keeps on going to get all the unique chars loll.
-
-
 #s = input("s = ") ... oh in leetcode you don't specify this
 s2 = s.replace('"', '')
 solution = Solution()
